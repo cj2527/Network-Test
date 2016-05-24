9810963de783562d5bd4a137c41b21fc475f79dc
cj2527 <cj2527@163.com>
2016-05-24 22:05:35 +0800

    网络连接
    测试网络连接状态

---
+++ 7-6-1Autolayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  7-6-1Autolayout
+//
+//  Created by cj on 16/5/20.
+//  Copyright © 2016年 cj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 7-6-1Autolayout/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  7-6-1Autolayout
+//
+//  Created by cj on 16/5/20.
+//  Copyright © 2016年 cj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
